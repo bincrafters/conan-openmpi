c20a43e9c08a31df66520a754ea902616e451004
SSE4 <tomskside@gmail.com>
2019-02-22 05:01:21 +0700

    - version 2.0.4
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class OpenMPIConan(ConanFile):
     name = "openmpi"
-    version = "3.0.0"
+    version = "2.0.4"
     url = "https://github.com/bincrafters/conan-openmpi"
     description = "A High Performance Message Passing Library"
     license = "https://www.open-mpi.org/community/license.php"
